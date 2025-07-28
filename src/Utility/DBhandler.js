ae89f9c05845bda07a637b8ca429da6f45d9c9fb
Ayush singh <aayushssingh2003@gmail.com>
2025-07-28 23:09:34 +0530

    add error handler

---
+++ src/Utility/DBhandler.js
@@ -0,0 +1,10 @@
+const DBhandler = (Func) => {
+    return  (req , res , next) => {
+         Promise.resolve(Func(res , req , next)).catch((err) => next(err))
+    }
+}
+
+export default DBhandler;
+
+
+
