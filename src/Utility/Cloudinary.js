051a4822fed4a43d19bc62db3ac33de236163d95
Ayush singh <aayushssingh2003@gmail.com>
2025-08-01 12:43:49 +0530

    add cloudinary1

---
+++ src/Utility/Cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from "cloudinary";
+import fs from fs; 
+
+cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_NAME,
+        api_key:  process.env.CLOUDINARY_API_KEY,
+        api_secret: process.env.CLOUDINARY_API_SECRE
+    });
+
+
+   const fileupload = async (Filepath) => { try {
+    if(!Filepath) return Error;
+
+   const response = await cloudinary.uploader.upload(Filepath , {
+        resource_type : "auto"
+    })
+    console.log("File uploaded successfully" , response.url)
+    fs.unlinkSync(Filepath);
+    return response;
+   } catch (error) {
+   fs.unlinkSync(Filepath)
+   return null;
+   }
+   }  
+ 
+
+   export default fileupload;
