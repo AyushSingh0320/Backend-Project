0a668856e210e029f11aec06b18b6e63747e9707
Ayush singh <aayushssingh2003@gmail.com>
2025-08-05 00:45:41 +0530

    add logout controller

--- src/middleware/Auth.MW.js
+++ src/middleware/Auth.MW.js
@@ -1,23 +1,34 @@
+import jwt from "jsonwebtoken";
 import DBhandler from "../Utility/DBhandler,js"
 import Apierror from "../Utility/Error";
+import { User } from "../Models/user.model";
 
 
 
 
-const Auth = DBhandler(async (req , res , next) => 
+const Auth = DBhandler(async (req , _ , next) => 
     {
-        const token = req.cookies?.accessToken ||
-        req.header("Authorization")?.replace("Bearer " , "");
+       try {
+         const token = req.cookies?.accessToken ||
+         req.header("Authorization")?.replace("Bearer " , "");
+ 
+         if(!token){
+             throw new Apierror(401 , "Unauthorized Request")
+         }
+           
+         const decodedtoken = jwt.verify(token , process.env.ACCESS_TOKEN_SECRET);
 
-        if(!token){
-            throw new Apierror(401 , "Unauthorized Request")
-        }
-        
+         const user = await User.findById  (decodedtoken?._id).select("-Password -RefreshToken")
 
+         if(!user){
+            throw new Apierror(401 , "Invalod Access token" )
+         }
+
+         req.user = user;
+         next()
+         } catch (error) {
+         throw new Apierror(401 , error?.message || "Invalid access Token")
+         }
 })
 
-
-
-
-
-export default Auth
+export default Auth;
