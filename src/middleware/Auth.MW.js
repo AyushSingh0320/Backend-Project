e451cacc4a92cda20bf66c0fbff0e637bccc879d
Ayush singh <aayushssingh2003@gmail.com>
2025-08-04 23:56:46 +0530

    add auth middleware

--- src/middleware/Auth.MW.js
+++ src/middleware/Auth.MW.js
@@ -1,10 +1,17 @@
 import DBhandler from "../Utility/DBhandler,js"
+import Apierror from "../Utility/Error";
 
 
 
 
 const Auth = DBhandler(async (req , res , next) => 
     {
+        const token = req.cookies?.accessToken ||
+        req.header("Authorization")?.replace("Bearer " , "");
+
+        if(!token){
+            throw new Apierror()
+        }
 
 })
 
